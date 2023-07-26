cca36a21c27f6ebb9284f7bec06f9e8868a949b8
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-26 12:31:55 +0200

    feat: Added marios info component, styled template code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { UserComponent } from './components/user/user.component';
 import { HomeComponent } from './components/home/home.component';
+import { MariosInfoComponent } from './components/home/marios-info/marios-info.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    HomeComponent,
+    MariosInfoComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/home/marios-info/marios-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marios-info',
+  templateUrl: './marios-info.component.html',
+  styleUrls: ['./marios-info.component.scss']
+})
+export class MariosInfoComponent {
+
+}
