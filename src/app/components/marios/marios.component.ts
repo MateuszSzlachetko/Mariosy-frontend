0bf8b15f3be9c105c39525e3597a614dd38c7f4f
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-26 16:47:43 +0200

    feat: Added template for marios component

---
+++ src/app/components/marios/marios.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marios',
+  templateUrl: './marios.component.html',
+  styleUrls: ['./marios.component.scss'],
+})
+export class MariosComponent {}
