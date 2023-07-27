ae805100a153524915a7866276a5c0b2b64a47b5
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-27 15:58:32 +0200

    feat: Added not styled mock login page

---
+++ src/app/shared/login/login.component.ts
@@ -0,0 +1,24 @@
+import { UserService } from './../../core/services/user.service';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/core/interfaces/user.interface';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  @ViewChild('f') loginForm!: NgForm;
+
+  constructor(private userService: UserService) {}
+  onSubmit() {
+    const username = this.loginForm.value.username;
+
+    this.userService.getUserByUsername(username).subscribe((data) => {
+      const user: User = data;
+      console.log(data);
+      this.userService.saveCurrentUserCookie(user);
+    });
+  }
+}
