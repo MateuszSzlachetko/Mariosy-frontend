41915eba1d421abd93e5ef0334d4872355d8d4d6
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-28 14:48:11 +0200

    feat: Added dynamically loaded marioses in grid

--- src/app/shared/marios-grid/marios-grid.component.ts
+++ src/app/shared/marios-grid/marios-grid.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Marios } from 'src/app/core/interfaces/marios.interface';
 
 @Component({
   selector: 'app-marios-grid',
   templateUrl: './marios-grid.component.html',
-  styleUrls: ['./marios-grid.component.scss']
+  styleUrls: ['./marios-grid.component.scss'],
 })
 export class MariosGridComponent {
-
+  @Input() mariosArray: Marios[] = [];
 }
