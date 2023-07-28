5525028e54f6d0800d9e26b07a445c081c23c759
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-28 14:51:01 +0200

    feat: Added dynamically loaded received marioses

--- src/app/components/user/received/received.component.ts
+++ src/app/components/user/received/received.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { UserService } from 'src/app/core/services/user.service';
+import { Component, OnInit } from '@angular/core';
+import { Marios } from 'src/app/core/interfaces/marios.interface';
 
 @Component({
   selector: 'app-received',
   templateUrl: './received.component.html',
-  styleUrls: ['./received.component.scss']
+  styleUrls: ['./received.component.scss'],
 })
-export class ReceivedComponent {
+export class ReceivedComponent implements OnInit {
+  receivedMarios: Marios[] = [];
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.getUserReceivedMarios().subscribe((data) => {
+      this.receivedMarios = data;
+    });
+  }
 }
