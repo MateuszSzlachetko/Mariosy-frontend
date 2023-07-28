1ede1e563699f1515a23cc0f7d58feb6c9ee74f9
MateuszSzlachetko <mateusz.szlachetko@gmail.com>
2023-07-28 14:48:52 +0200

    feat: Added dynamically loaded content in marios component

--- src/app/components/marios/marios.component.ts
+++ src/app/components/marios/marios.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Marios } from 'src/app/core/interfaces/marios.interface';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-marios',
   templateUrl: './marios.component.html',
   styleUrls: ['./marios.component.scss'],
 })
-export class MariosComponent {}
+export class MariosComponent implements OnInit {
+  @Input() marios!: Marios;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUserById(this.marios.author).subscribe((data) => {
+      this.marios.author = data.username;
+    });
+  }
+}
